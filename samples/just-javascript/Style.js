1a80e92c27ed86c6d9e85abfd227308210d5c03a
isabelpalomar <isabel.palomarb@gmail.com>
2012-12-11 17:32:20 -0600

    playing with pandas

---
+++ samples/just-javascript/Style.js
@@ -0,0 +1,26 @@
+
+/**
+* Playing with backgrounds 
+*/
+
+
+//Change background in any body page, pandas, pandas everywhere
+url = 'http://images2.layoutsparks.com/1/237707/panda-hidden-default-green.jpg';
+document.body.style.backgroundImage= "url(" + url + ")";
+
+// Panda logo in google, so cute :3
+url = 'http://3.bp.blogspot.com/-mXLp1Rx7tTw/UBEq4R-mH5I/AAAAAAAABic/YvFNHmkcLoQ/s1600/Google+Panda+3.9.png';
+document.getElementById('hplogo').style.backgroundImage= "url(" + url + ")";
+
+// or
+obj =  documen.getElementById('hplogo');
+url = 'http://3.bp.blogspot.com/-mXLp1Rx7tTw/UBEq4R-mH5I/AAAAAAAABic/YvFNHmkcLoQ/s1600/Google+Panda+3.9.png';
+obj.style.backgroundImage= "url(" + url + ")";
+
+
+
+// Use the console and have fun, DOM is very sexy
+
+
+
+
