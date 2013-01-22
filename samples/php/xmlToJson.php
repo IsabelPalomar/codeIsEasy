36511f0f46bb8f23f5b43d52425b6726e698602b
Isabel Palomar <isabel.palomarb@gmail.com>
2013-01-21 23:58:28 -0600

    xml to Json <3
    really helpful when we need to parse some files in xml format to a
    beautiful json format :P .. thats it!

---
+++ samples/php/xmlToJson.php
@@ -0,0 +1,33 @@
+<?php
+
+/* This class help to convert any xml file (including kml..) to a beautiful Json Format :3
+ * @isabelpalomar ;)
+ * 21/01/2013
+ */
+
+class XmlToJson {
+
+    /**
+    * Function Parser: Get the url and convert the content in json format
+    * string $url
+    */
+    public function Parser( $url ) {
+
+        //get the file content 
+        $fileContent  = file_get_contents( $url );
+
+        //replace the newlines, returns and tabs.
+        $fileContent  = str_replace(array("\n", "\r", "\t"), '', $fileContent);
+        $fileContent  = trim(str_replace('"', "'", $fileContent));
+
+        //load the clean XML 
+        $simpleXml    = simplexml_load_string($fileContent);
+
+        //parse the xml in a Json Format
+        $json         = json_encode($simpleXml);
+
+        //return the final content (in json format)
+        return $json;
+    }
+}
+?>
